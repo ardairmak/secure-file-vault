ec5bb1632c353aaa81528889cc02806b8f62c939
Arda Irmak <ardairmak2003@gmail.com>
2024-12-11 22:37:57 +0300

    feature: padding and changing mode from cfb to cbc

--- vault/encryption.go
+++ vault/encryption.go
@@ -1,11 +1,13 @@
 package vault
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 
@@ -49,14 +51,16 @@ func EncryptData(key, plaintext []byte) ([]byte, error) {
     if err != nil {
         return nil, err
     }
+    plaintext = pkcs7Pad(plaintext, aes.BlockSize)
     ciphertext := make([]byte, aes.BlockSize + len(plaintext))
     iv := ciphertext[:aes.BlockSize]
+
     if _, err := io.ReadFull(rand.Reader, iv); err != nil {
         return nil, err
     }
 
-    stream := cipher.NewCFBEncrypter(block, iv)
-    stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+    mode := cipher.NewCBCEncrypter(block, iv)
+    mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
 
     return ciphertext, nil
 }
@@ -70,12 +74,51 @@ func DecryptData(key, ciphertext []byte) ([]byte, error) {
     if len(ciphertext) < aes.BlockSize {
         return nil, fmt.Errorf("ciphertext too short")
     }
+
     iv := ciphertext[:aes.BlockSize]
     ciphertext = ciphertext[aes.BlockSize:]
 
-    stream := cipher.NewCFBDecrypter(block, iv)
-    stream.XORKeyStream(ciphertext, ciphertext)
-    return ciphertext, nil
+    if len(ciphertext)%aes.BlockSize != 0 {
+        return nil, fmt.Errorf("ciphertext is not a multiple of the block size")
+    }
+
+    mode := cipher.NewCBCDecrypter(block, iv)
+    mode.CryptBlocks(ciphertext, ciphertext)
+
+    plaintext, err := pkcs7Unpad(ciphertext, aes.BlockSize)
+    if err != nil {
+        return nil, fmt.Errorf("failed to unpad ciphertext: %v", err)
+    }
+
+    return plaintext, nil
+}
+
+func pkcs7Pad(data []byte, blockSize int) []byte {
+    padding := blockSize - len(data)%blockSize
+    padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+    return append(data, padtext...)
+}
+
+func pkcs7Unpad(data []byte, blockSize int) ([]byte, error) {
+    if len(data) == 0 || len(data)%blockSize != 0 {
+        return nil, errors.New("invalid padding size")
+    }
+
+    padding := data[len(data)-1]
+    padLen := int(padding)
+
+    if padLen == 0 || padLen > blockSize {
+        return nil, fmt.Errorf("invalid padding 1: padLen=%d, blockSize=%d, dataLen=%d", padLen, blockSize, len(data))
+    }
+
+    padtext := data[len(data)-padLen:]
+    for i, v := range padtext {
+        if v != padding {
+            return nil, fmt.Errorf("invalid padding 2: padtext[%d]=%d, expected=%d", i, v, padding)
+        }
+    }
+
+    return data[:len(data)-padLen], nil
 }
 
 func HashData(data []byte) string {
